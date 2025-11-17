a639a353b38dc17ad859695634794bbf1659dbb3
Tom Bonness <105823687+TomBonness@users.noreply.github.com>
2025-11-17 06:53:49 -0500

    angular api service for flask backend

---
+++ frontend/src/app/services/api.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getHealth() {
+    return this.http.get<any>(`${this.baseUrl}/health`);
+  }
+
+  predict(features: number[]) {
+    return this.http.post<any>(`${this.baseUrl}/predict`, { features });
+  }
+
+  getMetrics() {
+    return this.http.get<any>(`${this.baseUrl}/metrics`);
+  }
+
+  getDrift() {
+    return this.http.get<any>(`${this.baseUrl}/drift`);
+  }
+
+  submitFeedback(predictionId: number, actualLabel: string) {
+    return this.http.post<any>(`${this.baseUrl}/feedback`, {
+      prediction_id: predictionId,
+      actual_label: actualLabel
+    });
+  }
+}
